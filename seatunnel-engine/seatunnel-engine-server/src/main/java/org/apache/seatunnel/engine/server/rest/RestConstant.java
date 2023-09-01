00e7e1a2ffc2b859433ed3f78d9412799e366891
chaorongzhi <chao_rongzhi3@venustech.com.cn>
2023-09-01 14:39:27 +0800

    [feat][Zeta] add get job status by http

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestConstant.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestConstant.java
@@ -45,6 +45,7 @@ public class RestConstant {
     public static final String SUBMIT_JOB_URL = "/hazelcast/rest/maps/submit-job";
     public static final String CANCEL_JOB_URL = "/hazelcast/rest/maps/cancel-job";
     public static final String ENCRYPT_CONFIG = "/hazelcast/rest/maps/encrypt-config";
+    public static final String GET_JOB_STATUS_URL = "/hazelcast/rest/maps/get-job-status";
 
     public static final String SYSTEM_MONITORING_INFORMATION =
             "/hazelcast/rest/maps/system-monitoring-information";
