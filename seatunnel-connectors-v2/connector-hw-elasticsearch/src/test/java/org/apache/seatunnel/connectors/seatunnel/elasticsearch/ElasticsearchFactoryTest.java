ceae95e5cf073b6af23a0d1a295bc29d9786e93d
chaorongzhi <chao_rongzhi3@venustech.com.cn>
2023-12-08 19:31:54 +0800

    feat(hw-es): mvn spotless:apply

--- seatunnel-connectors-v2/connector-hw-elasticsearch/src/test/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/ElasticsearchFactoryTest.java
+++ seatunnel-connectors-v2/connector-hw-elasticsearch/src/test/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/ElasticsearchFactoryTest.java
@@ -17,30 +17,11 @@
 
 package org.apache.seatunnel.connectors.seatunnel.elasticsearch;
 
-import org.apache.seatunnel.shade.com.typesafe.config.Config;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigList;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigMemorySize;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigMergeable;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigObject;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigOrigin;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigResolveOptions;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigValue;
-
-import org.apache.seatunnel.connectors.seatunnel.elasticsearch.hw.client.EsRestClient;
-import org.apache.seatunnel.connectors.seatunnel.elasticsearch.hw.dto.ElasticsearchClusterInfo;
 import org.apache.seatunnel.connectors.seatunnel.elasticsearch.hw.sink.ElasticsearchSinkFactory;
 import org.apache.seatunnel.connectors.seatunnel.elasticsearch.hw.source.ElasticsearchSourceFactory;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.time.Duration;
-import java.time.Period;
-import java.time.temporal.TemporalAmount;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class ElasticsearchFactoryTest {
 
