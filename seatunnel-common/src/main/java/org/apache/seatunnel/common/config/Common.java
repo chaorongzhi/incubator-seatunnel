91378891506d7803395f77580e798b1d345d2e01
chaorongzhi <chao_rongzhi3@venustech.com.cn>
2023-12-14 16:35:46 +0800

    feat(zeta): 修改seatunnel 加载connector路径

--- seatunnel-common/src/main/java/org/apache/seatunnel/common/config/Common.java
+++ seatunnel-common/src/main/java/org/apache/seatunnel/common/config/Common.java
@@ -131,7 +131,7 @@ public class Common {
 
     /** Plugin Connector Dir */
     public static Path connectorDir() {
-        return Paths.get(getSeaTunnelHome(), "connectors");
+        return Paths.get(getSeaTunnelHome(), "connectors", "seatunnel");
     }
 
     /** lib Dir */
